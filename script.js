33f3ce846001328e5a8aabea36fbaf9037e5fc15
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-18 19:30:56 +0600

    remove old setup

--- script.js
+++
@@ -1,4 +0,0 @@
-var userName = "Fahad Ahmad";
-var age = 24;
-var isMarried = false;
-console.log(userName, age, isMarried);
--- script.ts
+++
@@ -1,5 +0,0 @@
-let userName: string = "Fahad Ahmad";
-let age: number = 24;
-let isMarried: boolean = false;
-
-console.log(userName, age, isMarried);
