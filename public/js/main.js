57cd27e8c96c1ecaa6f84874cce30a961e6ba2f3
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-18 20:23:34 +0600

    Functional operation Done

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let country = "Bangladesh";
 country = 52;
+const ageCalc = (name, age) => {
+    return `Hi ${name}, you are ${age} years old`;
+};
+ageCalc("Fahad", 25);
+const info = (name) => {
+    console.log("Hlw Dev");
+};
+const test = (age) => {
+    throw new Error("");
+};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 let country:string | number = "Bangladesh";
-
 country=52;
+
+
+const ageCalc = (name: string, age: number): string => {
+    return `Hi ${name}, you are ${age} years old`;
+};
+
+ageCalc("Fahad", 25);
+
+const info = (name: string): void => {
+    console.log("Hlw Dev");
+};
+
+const test = (age: number): never => {
+    throw new Error("");
+}
